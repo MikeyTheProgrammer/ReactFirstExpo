b159dc003c94c7946458f39fd6d34f2608daf4d3
unknown <you@example.com>
2022-05-22 17:50:37 +0300

    before alert canceling

--- hi-world/src/App.js
+++ hi-world/src/App.js
@@ -1,21 +1,53 @@
 import { useRef } from "react";
+import './App.css';
 
 function App() {
   const a = useRef(0);
   const b = useRef(0);
-
+  window.alert = window.console.log
   const plusaction = () => {
     console.log(a.current.value);
     console.log(b.current.value);
-    alert(parseInt(a.current.value) + parseInt(b.current.value));
+  alert (parseInt(a.current.value) + parseInt(b.current.value));
   };
 
+  const Minusaction = () => {
+    console.log(a.current.value);
+    console.log(b.current.value);
+    alert(parseInt(a.current.value) - parseInt(b.current.value));
+  };
+
+  const MultiplyAction = () => {
+    console.log(a.current.value);
+    console.log(b.current.value);
+    alert(parseInt(a.current.value) * parseInt(b.current.value));
+  };
+  const DivideAction = () => {
+    console.log(a.current.value);
+    console.log(b.current.value);
+   alert (parseInt(a.current.value) / parseInt(b.current.value));
+  };
+  const ResetAction = () => {
+    a.current.value = null
+    b.current.value = null
+  };
   return (
     <div className="App">
-      <header >
-      <input type="number" ref={a} />
-      <input type="number" ref={b} />
-      <button onClick={plusaction}>Result</button>
+      <header className="App-header">
+      <h1> Enter the numbers Down</h1>
+      <div>
+      <input type="number" ref={a} placeholder="0" />
+      <input type="number" ref={b} placeholder="0" />
+
+      </div>
+      <div className="margin"><p></p></div>
+      <div>
+      <button onClick={plusaction}>+</button>
+      <button onClick={Minusaction}>-</button>
+      <button onClick={MultiplyAction}>*</button>
+      <button onClick={DivideAction}>/</button>
+      <button onClick={ResetAction}>CE</button>
+      </div>
       </header>
     </div>
   );
